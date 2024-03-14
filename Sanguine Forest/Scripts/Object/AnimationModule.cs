63c74fb0b35f2b02c32728fb0b2a8b2500bf9314
Iurii Kupreev <Kostishko94@gmail.com>
2024-03-14 13:20:12 +0000

    Animation

--- Sanguine Forest/Scripts/Object/AnimationModule.cs
+++ Sanguine Forest/Scripts/Object/AnimationModule.cs
@@ -1,22 +1,29 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Sanguine_Forest
 {
+
     /// <summary>
     /// Animation controller. Need a SpriteModule to know what picture it should control. 
     /// </summary>
     internal class AnimationModule : Module
     {
         private SpriteModule _spriteModule;
-        private Rectangle frameRectangle;
+        private SpriteSheetData _spriteSheetData;
+
+        private float animationSpeed;
+        private float animationTimer;
+
         
 
-        public AnimationModule(GameObject parent, Vector2 shift, Rectangle framRec, SpriteModule spriteModule) : base (parent, shift) 
+        public AnimationModule(GameObject parent, Vector2 shift, SpriteSheetData spriteSheetData, SpriteModule spriteModule) : base (parent, shift) 
         {
             _spriteModule = spriteModule;
-            frameRectangle = framRec;
+            animationSpeed = 1f;
+            
         }
 
 
@@ -25,13 +32,31 @@ namespace Sanguine_Forest
 
         }
 
-        public Rectangle GetFrameRectabgle()
+        public void Play(string sequence)
         {
-            return frameRectangle;
+            
         }
 
 
 
 
+
     }
+
+
+    public class SpriteSheetData
+    {
+        public Rectangle frameRec;
+        public int rows, columns;        
+        public Dictionary<string, int[]> animationSequence;
+
+        public SpriteSheetData(Rectangle frameRec, Dictionary<string, int[]> animationSequence)
+        {
+            this.frameRec = frameRec;            
+            this.animationSequence = animationSequence;
+
+        }
+
+    }
+
 }

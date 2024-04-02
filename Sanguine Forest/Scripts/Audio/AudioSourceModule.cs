40c14020b072b92f95503fcd3f00495667c66d23
Kostishko <74458480+Kostishko@users.noreply.github.com>
2024-04-02 21:19:50 +0100

    AudioManager
    Audio and sounds, system with playing sounds

--- Sanguine Forest/Scripts/Audio/AudioSourceModule.cs
+++ Sanguine Forest/Scripts/Audio/AudioSourceModule.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SharpDX.XAudio2;
 using System.Collections.Generic;
 
 namespace Sanguine_Forest
@@ -12,13 +13,28 @@ namespace Sanguine_Forest
     internal class AudioSourceModule : Module
     {
 
-        private Dictionary<string, SoundEffect> soundEffects;
+        private Dictionary<string, SoundEffectInstance> soundEffects;
+        
 
 
+        /// <summary>
+        /// Audio module contain sounds effect dictionary to play
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="shift"></param>
+        /// <param name="dictionary"></param>
+        public AudioSourceModule(GameObject parent, Vector2 shift, Dictionary<string, SoundEffectInstance> dictionary) : base(parent, shift) 
+        {
+            soundEffects = dictionary;
+            
 
-        public AudioSourceModule(GameObject parent, Vector2 shift) : base(parent, shift) { }
+        }
 
-
+        public void PlaySoundOnce(string soundName)
+        {
+            
+            AudioManager.PlaySound(soundEffects[soundName]);
+        }
 
     }
 }
---
+++ Sanguine Forest/Scripts/TestScripts/IuriiTestGameObject.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanguine_Forest.Scripts.TestScripts
+{
+    internal class IuriiTestGameObject : GameObject
+    {
+        public AudioSourceModule AudioSourceModule;
+
+        public Dictionary<string, SoundEffectInstance> sounds;
+
+        public IuriiTestGameObject(Vector2 position, float rotation, ContentManager content) : base(position, rotation) 
+        {
+            sounds = new Dictionary<string, SoundEffectInstance>();
+            sounds.Add("Run", content.Load<SoundEffect>("Sounds/Sound_Char_Run").CreateInstance());
+            sounds.Add("Jump", content.Load<SoundEffect>("Sounds/Sound_Char_Jump").CreateInstance());
+            sounds.Add("Climb", content.Load<SoundEffect>("Sounds/Sound_Char_Climb").CreateInstance());
+
+            AudioSourceModule = new AudioSourceModule(this, Vector2.Zero, sounds);
+        }
+
+
+        public void UpdateMe(KeyboardState currKeyboard, KeyboardState oldKeyboard)
+        {
+            if(currKeyboard.IsKeyDown(Keys.W) )
+            {
+                AudioSourceModule.PlaySoundOnce("Jump");
+            }
+
+            if(currKeyboard.IsKeyDown(Keys.D))
+            {
+                AudioSourceModule.PlaySoundOnce("Run");
+            }
+
+            if( currKeyboard.IsKeyDown(Keys.S))
+            {
+                AudioSourceModule.PlaySoundOnce("Climb");
+            }
+
+        }
+    }
+}

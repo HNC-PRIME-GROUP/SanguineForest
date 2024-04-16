1ae75f93799eb617e612b2fd95d999e1b622382a
rdfranet <rdfr.anet@hotmail.com>
2024-04-16 13:05:44 +0100

    Updated

--- Sanguine Forest/Scripts/Environment/Obstacle/Obstacle.cs
+++ Sanguine Forest/Scripts/Environment/Obstacle/Obstacle.cs
@@ -11,19 +11,41 @@ namespace Sanguine_Forest.Scripts.Environment.Obstacle
 {
     internal class Obstacle : GameObject
     {
-        private SpriteModule spriteModule; 
-        private int alcoholLevelThreshold; // Alcohol level at which this obstacle will be visible
+        private PlayerState playerState;
+        private Character playerCharacter;
+        private SpriteModule spriteModule;
+        private PhysicModule physicModule; // The collision detection module
+        private int alcoholLevelThreshold; // Alcoholic level required for this obstacle to be visible
 
-        public Obstacle(Vector2 position, float rotation, Texture2D texture, int alcoholLevelThreshold) : base(position, rotation)
+
+        public Obstacle(Vector2 position, float rotation, Texture2D texture, int alcoholLevelThreshold, Character PlayerCharacter)
+           : base(position, rotation)
         {
             this.spriteModule = new SpriteModule(this, Vector2.Zero, texture, Extentions.SpriteLayer.obstacles);
+            this.physicModule = new PhysicModule(this, Vector2.Zero, new Vector2(texture.Width, texture.Height));
+            this.physicModule.isPhysicActive = true;
             this.alcoholLevelThreshold = alcoholLevelThreshold;
+            this.playerCharacter = PlayerCharacter;  // Set the PlayerCharacter parameter
         }
         public void Draw(SpriteBatch spriteBatch, int currentAlcoholLevel)
         {
             if (currentAlcoholLevel >= alcoholLevelThreshold)
             {
+                // Delegate drawing to the spriteModule
                 spriteModule.DrawMe(spriteBatch);
+            }
+        }
+        public override void UpdateMe()
+        {
+            base.UpdateMe();
+            physicModule.UpdateMe();
+        }
+        // Override the collided method
+        public override void Collided(Collision collision)
+        {
+            if (collision.GetCollidedPhysicModule().GetParent() == playerCharacter)
+            {
+                playerState.IsAlive = false; // Mark the player as dead if collided with obstacle
             }
         }
     }

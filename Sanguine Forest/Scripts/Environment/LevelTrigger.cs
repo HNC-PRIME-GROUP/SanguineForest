6965faaa2eebcfa565ec8a696387d3bea1323c64
Kostishko <74458480+Kostishko@users.noreply.github.com>
2024-05-17 16:17:59 +0100

    Trigger creation

---
+++ Sanguine Forest/Scripts/Environment/LevelTrigger.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanguine_Forest.Scripts.Environment
+{
+    internal class LevelTrigger
+    {
+    }
+}
